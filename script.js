ea463261ed564a6677f7d24bdf075a52c950bfca
“sebastian7010” <sebastianssc01@gmial.com>
2024-09-28 20:15:25 -0500

    todos los cambios

--- script.js
+++ script.js
@@ -5,86 +5,96 @@ document.addEventListener('DOMContentLoaded', function() {
 const carousels = [{
         id: 0,
         products: [
-            { "id": 1, "name": "chocorramo miti", "price": 3600 },
-            { "id": 2, "name": "caja jet", "price": 45800 },
-            { "id": 3, "name": "caja nucita", "price": 9500 },
-            { "id": 4, "name": "bolsa bombombun X24", "price": 11900 },
-            { "id": 5, "name": "bolsa trululu", "price": 3300 },
-            { "id": 6, "name": "chocolate ferrero", "price": 35000 },
-            { "id": 7, "name": "kit kat x4", "price": 29999 },
-            { "id": 8, "name": "almendraz", "price": 13800 },
-            { "id": 9, "name": "festables", "price": 12000 },
-            { "id": 10, "name": "festables rojos", "price": 34999 },
-            { "id": 11, "name": "festables mani", "price": 39999 },
-            { "id": 12, "name": "festables mora", "price": 29999 },
-            { "id": 13, "name": "jumbo", "price": 45999 },
-            { "id": 14, "name": "caja nutella", "price": 14300 },
-            { "id": 15, "name": "caja lechera", "price": 26500 },
-            { "id": 16, "name": "caja pizza", "price": 12000 },
-            { "id": 17, "name": "choco brack", "price": 17800 },
-            { "id": 18, "name": "choco italo", "price": 9400 },
-            { "id": 19, "name": "tarro nucita", "price": 21900 },
-            { "id": 20, "name": "uvas chevres", "price": 36999 },
-            { "id": 21, "name": "kinder bueno", "price": 31999 },
-            { "id": 22, "name": "anillo barbi", "price": 26100 },
-            { "id": 23, "name": "bianchi", "price": 2500 },
-            { "id": 24, "name": "bolsa okaloca", "price": 17800 },
-            { "id": 25, "name": "bolsa jetgol", "price": 31800 },
-            { "id": 26, "name": "burbujet", "price": 4800 },
-            { "id": 27, "name": "cables ácidos", "price": 17800 },
-            { "id": 28, "name": "cables verdes", "price": 17800 },
-            { "id": 29, "name": "caja bubaloo mora", "price": 13900 },
-            { "id": 30, "name": "caja bubalu", "price": 13900 },
-            { "id": 31, "name": "caja kick", "price": 20200 },
-            { "id": 32, "name": "caja dr locko", "price": 39000 },
-            { "id": 33, "name": "caja traident mora", "price": 24700 },
-            { "id": 34, "name": "caja trident", "price": 24700 },
-            { "id": 35, "name": "caja trident x60", "price": 14700 },
-            { "id": 36, "name": "canelazo", "price": 11000 },
-            { "id": 37, "name": "caramelo nucita", "price": 15600 },
-            { "id": 38, "name": "chao fresa", "price": 11300 },
-            { "id": 39, "name": "chao limón", "price": 18100 },
-            { "id": 40, "name": "choco disck", "price": 18400 },
-            { "id": 41, "name": "choco stop", "price": 27800 },
-            { "id": 42, "name": "cintas ácidas", "price": 17900 },
-            { "id": 43, "name": "dulce zandía", "price": 18700 },
-            { "id": 44, "name": "espumoso blanco", "price": 21800 },
-            { "id": 45, "name": "espumoso chocolate", "price": 21500 },
-            { "id": 46, "name": "galletas nucita", "price": 1500 },
-            { "id": 47, "name": "mentas hielo", "price": 2000 },
-            { "id": 48, "name": "mix 3", "price": 73200 },
-            { "id": 49, "name": "mix 4", "price": 76300 },
-            { "id": 50, "name": "mix 6", "price": 15000 },
-            { "id": 51, "name": "okaloca bolsa xs", "price": 2000 },
-            { "id": 52, "name": "okaloca polvo caja", "price": 11500 },
-            { "id": 53, "name": "ponkis", "price": 2000 },
-            { "id": 54, "name": "rafael x3", "price": 6800 },
-            { "id": 55, "name": "revolcón unidad", "price": 800 },
-            { "id": 56, "name": "trident fresa", "price": 24700 },
-            { "id": 57, "name": "trident mix 4", "price": 24700 },
-            { "id": 58, "name": "trident caja", "price": 34999 },
-            { "id": 59, "name": "trululu g", "price": 30999 },
-            { "id": 60, "name": "almendras xl", "price": 29999 },
-            { "id": 61, "name": "caja stables", "price": 31999 },
-            { "id": 62, "name": "caja punch", "price": 32999 },
-            { "id": 63, "name": "jumbo naranja", "price": 34999 },
-            { "id": 64, "name": "festables crunch", "price": 34999 },
-            { "id": 65, "name": "festables mani", "price": 39999 },
-            { "id": 66, "name": "festables mora", "price": 29999 },
-            { "id": 67, "name": "hair heads", "price": 31999 },
-            { "id": 68, "name": "jumbo requipe", "price": 34999 },
-            { "id": 69, "name": "choco bueno", "price": 30999 },
-            { "id": 70, "name": "minikinder 4", "price": 31999 },
-            { "id": 71, "name": "nerds redonda", "price": 45999 },
-            { "id": 72, "name": "push pop", "price": 34999 },
-            { "id": 73, "name": "ring pop", "price": 39999 },
-            { "id": 74, "name": "tetero mora", "price": 29999 },
-            { "id": 75, "name": "tetero azul", "price": 31999 },
-            { "id": 76, "name": "tetero rosa", "price": 34999 },
-            { "id": 77, "name": "wuba azul", "price": 36999 },
-            { "id": 78, "name": "wuba fresa", "price": 29999 },
-            { "id": 79, "name": "caja ancheta", "price": 89999 },
-            { "id": 80, "name": "ancheta roja", "price": 34999 }
+            { "id": 1, "name": "chocolate ferrero rocher", "price": 3600 },
+            { "id": 2, "name": "caja jumbo", "price": 45800 },
+            { "id": 3, "name": "caja jet", "price": 9500 },
+            { "id": 4, "name": "choco breack", "price": 11900 },
+            { "id": 5, "name": "bon bon bum ", "price": 3300 },
+            { "id": 6, "name": "bolsa trululu", "price": 35000 },
+            { "id": 7, "name": "caja festables", "price": 29999 },
+            { "id": 8, "name": "festables milk chocolate", "price": 13800 },
+            { "id": 9, "name": "festables penut", "price": 12000 },
+            { "id": 10, "name": "festables milk", "price": 34999 },
+            { "id": 11, "name": "festables crunch", "price": 39999 },
+            { "id": 12, "name": "almendraz", "price": 29999 },
+            { "id": 13, "name": "bianchi", "price": 45999 },
+            { "id": 14, "name": "jet gol", "price": 14300 },
+            { "id": 15, "name": "burbu jet", "price": 26500 },
+            { "id": 16, "name": "bubaloo mora", "price": 12000 },
+            { "id": 17, "name": "caja nucita", "price": 17800 },
+            { "id": 18, "name": "jumbo arequipe", "price": 9400 },
+            { "id": 19, "name": "jumbo mani", "price": 21900 },
+            { "id": 20, "name": "almenras xs", "price": 36999 },
+            { "id": 21, "name": "caja lechera", "price": 31999 },
+            { "id": 22, "name": "chocorramo mini", "price": 26100 },
+            { "id": 23, "name": "caramelo nucita", "price": 2500 },
+            { "id": 24, "name": "chocoramo miti", "price": 17800 },
+            { "id": 25, "name": "mentas de hielo", "price": 31800 },
+            { "id": 26, "name": "bolsa okaloca", "price": 4800 },
+            { "id": 27, "name": "kit kat 4", "price": 17800 },
+            { "id": 28, "name": "anillos de barbi", "price": 17800 },
+            { "id": 29, "name": "cable acido rojo", "price": 13900 },
+            { "id": 30, "name": "cable acido verde", "price": 13900 },
+            { "id": 31, "name": "boobalo verde", "price": 20200 },
+            { "id": 32, "name": "kick mani", "price": 39000 },
+            { "id": 33, "name": "dr loco", "price": 24700 },
+            { "id": 34, "name": "mani la especial", "price": 24700 },
+            { "id": 35, "name": "bon bon bum surtido", "price": 14700 },
+            { "id": 36, "name": "caja nucita avellana", "price": 11000 },
+            { "id": 37, "name": "caja nutella", "price": 15600 },
+            { "id": 38, "name": "caja piazza", "price": 11300 },
+            { "id": 39, "name": "caja sour punch", "price": 18100 },
+            { "id": 40, "name": "caja rafaello", "price": 18400 },
+            { "id": 41, "name": "splot acido", "price": 27800 },
+            { "id": 42, "name": "splot acido varios ", "price": 17900 },
+            { "id": 43, "name": "caja traident mora", "price": 18700 },
+            { "id": 44, "name": "caja traident surtido", "price": 21800 },
+            { "id": 45, "name": "bolsa anisadas", "price": 21500 },
+            { "id": 46, "name": "caja mora traident", "price": 1500 },
+            { "id": 47, "name": "caja canelazo", "price": 2000 },
+            { "id": 48, "name": "tarro nucita", "price": 73200 },
+            { "id": 49, "name": "caja trident tutifruti", "price": 76300 },
+            { "id": 50, "name": "chao fresa", "price": 15000 },
+            { "id": 51, "name": "chao limon", "price": 2000 },
+            { "id": 52, "name": "chocobreack frutal", "price": 11500 },
+            { "id": 53, "name": "choco Disk", "price": 2000 },
+            { "id": 54, "name": "italo choco baby", "price": 6800 },
+            { "id": 55, "name": "choco stop", "price": 800 },
+            { "id": 56, "name": "cintas acidas", "price": 24700 },
+            { "id": 57, "name": "melancia zandia", "price": 24700 },
+            { "id": 58, "name": "espumoso xs", "price": 34999 },
+            { "id": 59, "name": "espumoso chocolate blanck", "price": 30999 },
+            { "id": 60, "name": "espumoso chocolate blanck rose", "price": 29999 },
+            { "id": 61, "name": "gallete nucita", "price": 31999 },
+            { "id": 62, "name": "air heads", "price": 32999 },
+            { "id": 63, "name": "kinder bueno", "price": 34999 },
+            { "id": 64, "name": "bolsa moritas", "price": 34999 },
+            { "id": 65, "name": "mini kinder x4", "price": 39999 },
+            { "id": 66, "name": "kit mix chocolates", "price": 29999 },
+            { "id": 67, "name": "espumoso chocolate blanck + mix chocolates y gomitas", "price": 31999 },
+            { "id": 68, "name": "mix 6 bolsas de dulces", "price": 34999 },
+            { "id": 69, "name": "lechera x3", "price": 30999 },
+            { "id": 70, "name": "gomitas grisley", "price": 31999 },
+            { "id": 71, "name": "dulce snow", "price": 45999 },
+            { "id": 72, "name": "nerds caja", "price": 34999 },
+            { "id": 73, "name": "okaloca bolsa", "price": 39999 },
+            { "id": 74, "name": "chicle en polvo", "price": 29999 },
+            { "id": 75, "name": "peluche tigrillo", "price": 31999 },
+            { "id": 76, "name": "tira de ponkis", "price": 34999 },
+            { "id": 77, "name": "push poop azul", "price": 36999 },
+            { "id": 78, "name": "kit rafaello x3", "price": 29999 },
+            { "id": 79, "name": "bolsa revolcon", "price": 89999 },
+            { "id": 80, "name": "anillo ring pop", "price": 34999 },
+            { "id": 81, "name": "tetero morado", "price": 31999 },
+            { "id": 82, "name": "tetero azul", "price": 34999 },
+            { "id": 82, "name": "tetero rosa", "price": 36999 },
+            { "id": 83, "name": "trident mix4", "price": 29999 },
+            { "id": 84, "name": "trid caja ", "price": 89999 },
+            { "id": 85, "name": "trululu karol g", "price": 34999 },
+            { "id": 86, "name": "tetero morado", "price": 31999 },
+            { "id": 87, "name": "uvas chevres", "price": 34999 },
+            { "id": 88, "name": "chicle azul", "price": 36999 },
+            { "id": 89, "name": "chicle rosa", "price": 29999 }
 
         ],
         currentPage: 0,
@@ -93,56 +103,46 @@ const carousels = [{
     {
         id: 1,
         products: [
-            { id: 81, name: "7 cera capilar", price: 79999 },
-            { id: 82, name: "labial", price: 89999 },
-            { id: 83, name: "bolso tomy", price: 69999 },
-            { id: 84, name: "splash body", price: 79999 },
-            { id: 85, name: "cajas", price: 49999 },
-            { id: 86, name: "cámara facial", price: 69999 },
-            { id: 87, name: "cartera tomy", price: 39999 },
-            { id: 88, name: "cera capilar", price: 59999 },
-            { id: 89, name: "cera kit capilar", price: 99999 },
-            { id: 90, name: "gel de ducha", price: 49999 },
-            { id: 91, name: "jabón baño", price: 54999 },
-            { id: 92, name: "jabón baño 2", price: 29999 },
-            { id: 93, name: "jabón baño 3", price: 19999 },
-            { id: 94, name: "jabón baño 5", price: 79999 },
-            { id: 95, name: "jabón baño 6", price: 24999 },
-            { id: 96, name: "jabón baño 7", price: 14999 },
-            { id: 97, name: "jabón baño 8", price: 89999 },
-            { id: 98, name: "jabón baño 9", price: 34999 },
-            { id: 99, name: "kit 9 cera capilar", price: 99999 },
-            { id: 100, name: "labial", price: 10999 },
-            { id: 101, name: "labial rosa", price: 8999 },
-            { id: 102, name: "loción body", price: 11999 },
-            { id: 103, name: "masajeador capilar", price: 16999 },
-            { id: 104, name: "moña 1", price: 59999 },
-            { id: 105, name: "moña 10", price: 79999 },
-            { id: 106, name: "moña 11", price: 19999 },
-            { id: 107, name: "moña 2", price: 45999 },
-            { id: 108, name: "moña 3", price: 32999 },
-            { id: 109, name: "moña 4", price: 29999 },
-            { id: 110, name: "moña 5", price: 26999 },
-            { id: 111, name: "moña 6", price: 37999 },
-            { id: 112, name: "moña 7", price: 19999 },
-            { id: 113, name: "moña 9", price: 45999 },
-            { id: 114, name: "moñas", price: 37999 },
-            { id: 115, name: "splash body", price: 19999 },
-            { id: 116, name: "splash body 2", price: 31999 },
-            { id: 117, name: "splash body 3", price: 31999 },
-            { id: 118, name: "splash body 4", price: 34999 },
-            { id: 119, name: "splash body 5", price: 34999 },
-            { id: 120, name: "splash body 6", price: 36999 },
-            { id: 121, name: "toallita aironman", price: 36999 },
-            { id: 122, name: "toallita capitán", price: 39999 },
-            { id: 123, name: "toallita encanto", price: 39999 },
-            { id: 124, name: "toallita mikie", price: 39999 },
-            { id: 125, name: "toallita mini", price: 41999 },
-            { id: 126, name: "toallita princesa", price: 41999 },
-            { id: 127, name: "toallita spiderman", price: 43999 },
-            { id: 128, name: "toallita thor", price: 43999 },
-            { id: 129, name: "tomy verde", price: 45999 },
-            { id: 130, name: "tomy azul", price: 45999 }
+            { id: 90, name: "bolso tomy veerde", price: 0 },
+            { id: 91, name: "bolso tomy blanco", price: 0 },
+            { id: 92, name: "bolso tomy azul", price: 0 },
+            { id: 93, name: "caja rosa o blanca", price: 0 },
+            { id: 94, name: "facial", price: 0 },
+            { id: 125, name: "moñas", price: 0 },
+            { id: 126, name: "spray 1", price: 0 },
+            { id: 127, name: "spray 2", price: 0 },
+            { id: 128, name: "spray 3", price: 0 },
+            { id: 129, name: "spray 4", price: 0 },
+            { id: 130, name: "spray 5", price: 0 },
+            { id: 96, name: "bolso xs tomy azul", price: 0 },
+            { id: 97, name: "cera capilar", price: 0 },
+            { id: 98, name: "cera capilar +", price: 0 },
+            { id: 99, name: "", price: 0 },
+            { id: 100, name: "", price: 0 },
+            { id: 101, name: "", price: 0 },
+            { id: 102, name: "", price: 0 },
+            { id: 103, name: "", price: 0 },
+            { id: 104, name: "", price: 0 },
+            { id: 105, name: "", price: 0 },
+            { id: 106, name: "", price: 0 },
+            { id: 109, name: "", price: 0 },
+            { id: 110, name: "", price: 0 },
+            { id: 111, name: "", price: 0 },
+            { id: 112, name: "", price: 0 },
+            { id: 113, name: "", price: 0 },
+            { id: 114, name: "", price: 0 },
+            { id: 115, name: "", price: 0 },
+            { id: 116, name: "", price: 0 },
+            { id: 117, name: "", price: 0 },
+            { id: 118, name: "", price: 0 },
+            { id: 119, name: "", price: 0 },
+            { id: 120, name: "", price: 0 },
+            { id: 121, name: "", price: 0 },
+            { id: 122, name: "", price: 0 },
+            { id: 123, name: "", price: 0 },
+            { id: 124, name: "", price: 0 },
+
+
         ],
 
         currentPage: 0,
@@ -159,7 +159,6 @@ const carousels = [{
             { id: 136, name: "ancheta baso 2", price: 89999 },
             { id: 137, name: "ancheta baso 3", price: 69999 },
             { id: 138, name: "ancheta baso 4", price: 89999 },
-            { id: 139, name: "ancheta roja", price: 89999 },
 
         ],
         currentPage: 0,
@@ -196,17 +195,44 @@ function buyProduct(productName, button) {
         const carouselId = productCard.closest('.carousel').id.split('-')[1];
         const product = carousels[carouselId].products.find(p => p.id == productId);
 
-        const message = encodeURIComponent(
-            `Hola, quiero comprar ${quantity} unidades de ${productName} a un precio de ${product.price.toLocaleString()} COP cada una.`
-        );
+        if (!cart[carouselId]) {
+            cart[carouselId] = {};
+        }
 
-        const whatsappURL = `https://wa.me/573237342020?text=${message}`;
+        cart[carouselId][productId] = { name: product.name, quantity: quantity, price: product.price };
+        updateCartItemCount();
 
-        window.open(whatsappURL, '_blank');
+        sendCombinedWhatsAppMessage();
     } else {
         alert('Por favor, seleccione al menos una unidad para comprar.');
     }
 }
+
+function sendCombinedWhatsAppMessage() {
+    if (Object.keys(cart).length === 0) {
+        alert("El carrito está vacío. Agrega productos antes de realizar una compra.");
+        return;
+    }
+
+    let message = "Hola, quiero comprar los siguientes productos:\n";
+    let totalPrice = 0;
+
+    for (let carouselId in cart) {
+        for (let productId in cart[carouselId]) {
+            const product = cart[carouselId][productId];
+            message += `- ${product.name}: ${product.quantity} unidades, Precio: ${product.price}\n`;
+            totalPrice += product.quantity * product.price;
+        }
+    }
+
+    message += `\nPrecio total: ${totalPrice.toLocaleString()} COP`;
+
+    const encodedMessage = encodeURIComponent(message);
+    const whatsappURL = `https://wa.me/573237342020?text=${encodedMessage}`;
+
+    window.open(whatsappURL, '_blank');
+}
+
 
 
 function renderCarousel(carousel) {
@@ -343,7 +369,7 @@ document.addEventListener('DOMContentLoaded', function() {
     hamburgerButton.addEventListener('click', function() {
         navbarMenu.classList.toggle('active');
         if (navbarMenu.classList.contains('active')) {
-            mainContent.style.marginTop = '60vh'; // Ajusta la altura si es necesario
+            mainContent.style.marginTop = '60vh';
         } else {
             mainContent.style.marginTop = '0';
         }
